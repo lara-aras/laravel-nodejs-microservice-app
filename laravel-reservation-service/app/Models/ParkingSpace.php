64405780101e9c056cb952ad31e46d4aa35fc97b
Lara Aras <lara.aras@gmail.com>
2024-05-08 21:22:03 +0200

    chore: Add ParkingSpace model and migration

---
+++ laravel-reservation-service/app/Models/ParkingSpace.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ParkingSpace extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+}
