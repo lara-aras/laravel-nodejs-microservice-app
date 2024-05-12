8fc46d44bbffe05cc4d9c481632955f7e3b697c5
Lara Aras <lara.aras@gmail.com>
2024-05-12 19:49:15 +0200

    refactor: Update email service configuration and add new email addresses

--- nodejs-email-service/config/config.js
+++ nodejs-email-service/config/config.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   databasePath: process.env.SQLITE_DATABASE_PATH,
+  reservationsEmailAddress: process.env.RESERVATIONS_EMAIL_ADDRESS,
+  supportEmailAddress: process.env.SUPPORT_EMAIL_ADDRESS,
 };
