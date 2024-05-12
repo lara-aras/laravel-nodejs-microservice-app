b58d9496603b0975811f12095d0bb1a63f1c2678
Lara Aras <lara.aras@gmail.com>
2024-05-12 22:11:00 +0200

    refactor: Refactor email service database schema

--- nodejs-email-service/src/database/database.js
+++ nodejs-email-service/src/database/database.js
@@ -5,16 +5,30 @@ const db = new sqlite3.Database(config.databasePath);
 
 db.serialize(function () {
   db.run(
-    "CREATE TABLE IF NOT EXISTS sent_emails (recipient TEXT NOT NULL, subject TEXT NOT NULL, html TEXT NOT NULL, date_sent TEXT NOT NULL)"
+    `CREATE TABLE IF NOT EXISTS sent_emails (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      recipient VARCHAR(255) NOT NULL,
+      subject VARCHAR(255) NOT NULL,
+      html TEXT NOT NULL,
+      date_sent DATETIME DEFAULT CURRENT_TIMESTAMP
+    )`
   );
   db.run(
-    "CREATE TABLE IF NOT EXISTS failed_emails (recipient TEXT NOT NULL, subject TEXT NOT NULL, html TEXT, error TEXT NOT NULL, date_failed TEXT NOT NULL)"
+    `CREATE TABLE IF NOT EXISTS failed_emails (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      recipient VARCHAR(255) NOT NULL,
+      subject VARCHAR(255) NOT NULL,
+      html TEXT,
+      error TEXT NOT NULL,
+      date_failed DATETIME DEFAULT CURRENT_TIMESTAMP
+    )`
   );
 });
 
 module.exports = {
   storeEmailSendSuccess: (recipient, subject, html) => {
     const date_sent = new Date().toISOString();
+
     db.run(
       "INSERT INTO sent_emails VALUES (?, ?, ?, ?)",
       recipient,
@@ -27,6 +41,7 @@ module.exports = {
     const date_failed = new Date().toISOString();
     db.run(
       "INSERT INTO failed_emails VALUES (?, ?, ?, ?, ?)",
+      
       recipient,
       subject,
       html,
